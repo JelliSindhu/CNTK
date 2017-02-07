61dac3914289c42fea75b97a62512dbef76cfc0d
Zhou Wang <zhouwang@microsoft.com>
2017-02-07 11:41:07 +0100

    fix typo

--- bindings/csharp/CNTKLibraryManagedDll/CNTKLibraryManagedDll-GPU-AssemblyInfo.cs
+++ bindings/csharp/CNTKLibraryManagedDll/CNTKLibraryManagedDll-GPU-AssemblyInfo.cs
@@ -2,7 +2,7 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
 //
-// CNTKLibraryManagedDll-CPUONly-AssemblyInfo.cpp -- Provides Strong Name for CNTKLibraryManagedDll (CPUOnly flavor) assembly.
+// CNTKLibraryManagedDll-GPU-AssemblyInfo.cpp -- Provides Strong Name for CNTKLibraryManagedDll (GPU flavor) assembly.
 // 
 // Note that the key pair (inlcuding secret key) is stored in the same folder as this source file.
 // The Strong Name serves for identity puposes and for the applications that require it.
